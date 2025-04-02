4a62c3c3edfe336788ceb894f67851b0d0ffc5e5
nausad <na2439@it.jgec.ac.in>
2025-04-02 23:43:59 +0530

    polymorphism , getter ,setter

--- polymorphism/Program.cs
+++ polymorphism/Program.cs
@@ -40,21 +40,117 @@ namespace polymorphism
         }
 
     }
+
+    public class Student
+    {
+        private int _id;
+        private string _name;
+        private int _pass = 55;
+        private string _city;
+        
+
+
+        public string Email
+        {
+            get; set;
+        }
+        public string city
+        {
+            set
+            {
+                this._city = value;
+            }
+            get
+            {
+                return this._city;
+            }
+        }
+
+        public int pass
+        {
+            get
+            {
+                return this._pass;
+            }
+            
+        }
+
+        //public void SetId(int id)
+        //{
+        //    if (id <= 0)
+        //    {
+        //        throw new Exception("Student id cannot be negative");
+        //    }
+        //    this.id = id; 
+        //}
+        public int Id
+        {
+            set {
+                if ( value<= 0)
+                {
+                    throw new Exception("Student id cannot be negative");
+                }
+
+                this._id = value;
+            }
+            get
+            {
+                return this._id;
+            }
+          
+        }
+
+        //public int GetId()
+        //{
+        //    return this.id;
+        //}
+        public string Name
+        {
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new Exception("name cannot be numm or empty");
+                }
+                this._name = value;
+
+            }
+            get
+            {
+                return string.IsNullOrEmpty(this._name) ? "noname" : this._name;
+
+
+            }
+
+        }
+        
+
+
+
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Employee[] emp = new Employee[4];
+            //Employee[] emp = new Employee[4];
 
-            emp[0] = new Employee();
-            emp[1] = new PartTime();
-            emp[2] = new FullTime();
-            emp[3] = new TempTime();
+            //emp[0] = new Employee();
+            //emp[1] = new PartTime();
+            //emp[2] = new FullTime();
+            //emp[3] = new TempTime();
 
-            foreach(Employee  e in emp)
-            {
-                e.PrintFullName();
-            }
+            //foreach(Employee  e in emp)
+            //{
+            //    e.PrintFullName();
+            //}
+
+            Student s = new Student();
+            s.Id = 3;
+            s.Name ="montu";
+            s.city = "burdwan";
+            s.Email = "montu@gmail.com";
+
+            Console.WriteLine("student id = {0}, name = {1}, pass={2}, email = {3}, city={4}", s.Id,s.Name,s.pass,s.Email,s.city);
 
 
         }
